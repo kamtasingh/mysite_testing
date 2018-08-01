0a297ad003544c8998cd9b57f5d4b5c62306f28d
kamtasingh <37369042+kamtasingh@users.noreply.github.com>
2018-08-01 15:43:10 +0530

    login url change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from .views import *
 urlpatterns = [
-    url(r'^accounts/login/$', login_site, name='login'),
+    url(r'^$', login_site, name='login'),
     url(r'^accounts/logout/$', auth_logout, name='auth-logout'),
     url(r'^accounts/register/$', registration, name='site-register'),
     url(r'^mysite/$', mysite, name='mysite'),
